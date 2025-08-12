be434075114d11b820128e57c9689c04526ad143
tbweaver <thomasbartonweaver@gmail.com>
2025-08-11 22:38:09 -0500

    first commit

---
+++ app/src/main/java/com/example/letstryagain/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.letstryagain
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun buttonClick(view: View?){
+        println("Hey guys, it's me again.")
+    }
+}
